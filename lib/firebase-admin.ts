e70b440e7c499fa103e8ffa5b274104e25906e9c
Dawn-Of-Justice <salosoja@gmail.com>
2026-04-12 19:55:10 +0530

    chore: delete dead whatsapp service, add firebase-admin guard, document env vars

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,8 +2,11 @@ import { cert, getApps, initializeApp } from 'firebase-admin/app'
 import { getMessaging } from 'firebase-admin/messaging'
 
 if (!getApps().length) {
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+    throw new Error('Missing FIREBASE_SERVICE_ACCOUNT_KEY environment variable')
+  }
   initializeApp({
-    credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!)),
+    credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)),
   })
 }
 
